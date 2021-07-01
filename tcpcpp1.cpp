7516e9500b8745b76b5d2d0b1def7d0468189d7b
JuneSungAn <43237393+ajs3801@users.noreply.github.com>
2021-07-02 00:31:15 +0900

    Update and rename tcpcppday1.cpp to tcpcpp1.cpp

---
+++ tcpcpp1.cpp
@@ -0,0 +1,23 @@
+//http://tcpschool.com/cpp/intro <I'm studyint cpp in tcpschool.com in webpage.
+
+//main code----------------------------------------------------------------------------------------- 
+#include<iostream>
+#define TEXT "Hello, world!!"
+
+int main(void)
+//Input doesn't exist and output is integer. So, this main function will return 0 as return value.
+{
+	std::cout << "Hello, world!";
+	std::cout << "\n";
+	std::cout << TEXT;
+	return 0; // As I mentioned before, output is integer. So, it returns 0 as return value.
+}
+//main code end--------------------------------------------------------------------------------------
+
+
+/* [result console]
+Hello, world!
+Hello, world!!
+*/
+
+
