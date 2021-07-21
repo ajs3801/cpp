18f569fd3c6ae38648cb90d919005bf14e15e04e
JuneSungAn <43237393+ajs3801@users.noreply.github.com>
2021-07-21 23:09:07 +0900

    Create 1007.cpp

---
+++ CodeUp-100/1007.cpp
@@ -0,0 +1,16 @@
+/* CodeUp-1007*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	cout << "C:\\Download\\hello.cpp";
+
+	return 0;
+}
+
+/*
+[Console Result]
+C:\Download\hello.cpp
+*/
