15cb5f7e3f7d64959b6967868a926516e9007126
JuneSungAn <43237393+ajs3801@users.noreply.github.com>
2021-07-21 23:16:01 +0900

    Create 1010.cpp

---
+++ CodeUp-100/1010.cpp
@@ -0,0 +1,20 @@
+/* CodeUp-1009*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int UserNumber;
+
+	cin >> UserNumber;
+	cout << UserNumber << endl;
+
+	return 0;
+}
+
+/*
+[Console Result]
+15
+15
+*/
