d99aaf67465c6f1f84b9dfc96f1e9952fbb7b87c
JuneSungAn <43237393+ajs3801@users.noreply.github.com>
2021-07-21 23:04:52 +0900

    Create 1006.cpp

---
+++ CodeUp-100/1006.cpp
@@ -0,0 +1,17 @@
+/* CodeUp-1005*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+  //특수문자 %는 %%로 써야되는거 아니었나...
+	cout << "\"!@#$%^&*()\"";
+
+	return 0;
+}
+
+/*
+[Console Result]
+"!@#$%^&*()"
+*/
